ee9ebbc76b830b28571928b00049ac53adc59175
Elena <vodopyan.elena@gmail.com>
2022-09-16 11:52:48 +0300

    Example_2

---
+++ Example_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+string str = GetNumber("Введите число для нахождения его сумм цифр");
+int sum = Sumfan(str);
+Console.WriteLine(sum);
+
+string GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+
+int Sumfan(string str)
+{
+    int sum = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(Convert.ToString(str[i]));
+    }
+    return sum;
+}
